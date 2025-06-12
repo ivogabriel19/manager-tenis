53e02a21af3d8874967a12a07ce40fc8222448e3
ivogabreil19 <ivodegange@gmail.com>
2025-06-11 22:41:38 -0300

    formatting

--- src/utils/interact.js
+++ src/utils/interact.js
@@ -1,189 +1,42 @@
 export function getPlayers() {
-  let name1 =
-    document.getElementById("jugador1").options[
-      document.getElementById("jugador1").selectedIndex
-    ].value;
-  let name2 =
-    document.getElementById("jugador2").options[
-      document.getElementById("jugador2").selectedIndex
-    ].value;
-  let surf1 =
-    document.getElementById("surface1").options[
-      document.getElementById("surface1").selectedIndex
-    ].value;
-  let surf2 =
-    document.getElementById("surface2").options[
-      document.getElementById("surface2").selectedIndex
-    ].value;
-  let nomb1 =
-    document.getElementById("jugador1").options[
-      document.getElementById("jugador1").selectedIndex
-    ].text;
-  let nomb2 =
-    document.getElementById("jugador2").options[
-      document.getElementById("jugador2").selectedIndex
-    ].text;
-  let fore1 = parseInt(
-    document.getElementById("forehand1").options[
-      document.getElementById("forehand1").selectedIndex
-    ].value
-  );
-  let fore2 = parseInt(
-    document.getElementById("forehand2").options[
-      document.getElementById("forehand2").selectedIndex
-    ].value
-  );
-  let back1 = parseInt(
-    document.getElementById("backhand1").options[
-      document.getElementById("backhand1").selectedIndex
-    ].value
-  );
-  let back2 = parseInt(
-    document.getElementById("backhand2").options[
-      document.getElementById("backhand2").selectedIndex
-    ].value
-  );
-  let vol1 = parseInt(
-    document.getElementById("volley1").options[
-      document.getElementById("volley1").selectedIndex
-    ].value
-  );
-  let vol2 = parseInt(
-    document.getElementById("volley2").options[
-      document.getElementById("volley2").selectedIndex
-    ].value
-  );
-  let drop1 = parseInt(
-    document.getElementById("dropshot1").options[
-      document.getElementById("dropshot1").selectedIndex
-    ].value
-  );
-  let drop2 = parseInt(
-    document.getElementById("dropshot2").options[
-      document.getElementById("dropshot2").selectedIndex
-    ].value
-  );
-  let spe1 = parseInt(
-    document.getElementById("speed1").options[
-      document.getElementById("speed1").selectedIndex
-    ].value
-  );
-  let spe2 = parseInt(
-    document.getElementById("speed2").options[
-      document.getElementById("speed2").selectedIndex
-    ].value
-  );
-  let sta1 = parseInt(
-    document.getElementById("stamina1").options[
-      document.getElementById("stamina1").selectedIndex
-    ].value
-  );
-  let sta2 = parseInt(
-    document.getElementById("stamina2").options[
-      document.getElementById("stamina2").selectedIndex
-    ].value
-  );
-  let ser1 = parseInt(
-    document.getElementById("service1").options[
-      document.getElementById("service1").selectedIndex
-    ].value
-  );
-  let ser2 = parseInt(
-    document.getElementById("service2").options[
-      document.getElementById("service2").selectedIndex
-    ].value
-  );
-  let pow1 = parseInt(
-    document.getElementById("power1").options[
-      document.getElementById("power1").selectedIndex
-    ].value
-  );
-  let pow2 = parseInt(
-    document.getElementById("power2").options[
-      document.getElementById("power2").selectedIndex
-    ].value
-  );
-  let rest1 = parseInt(
-    document.getElementById("resto1").options[
-      document.getElementById("resto1").selectedIndex
-    ].value
-  );
-  let rest2 = parseInt(
-    document.getElementById("resto2").options[
-      document.getElementById("resto2").selectedIndex
-    ].value
-  );
-  let consi1 = parseInt(
-    document.getElementById("consistencia1").options[
-      document.getElementById("consistencia1").selectedIndex
-    ].value
-  );
-  let consi2 = parseInt(
-    document.getElementById("consistencia2").options[
-      document.getElementById("consistencia2").selectedIndex
-    ].value
-  );
-  let form1 = parseInt(
-    document.getElementById("form1").options[
-      document.getElementById("form1").selectedIndex
-    ].value
-  );
-  let form2 = parseInt(
-    document.getElementById("form2").options[
-      document.getElementById("form2").selectedIndex
-    ].value
-  );
-
-  let technique1 = parseInt(
-    document.getElementById("technique1").options[
-      document.getElementById("technique1").selectedIndex
-    ].value
-  );
-  let technique2 = parseInt(
-    document.getElementById("technique2").options[
-      document.getElementById("technique2").selectedIndex
-    ].value
-  );
-  let position1 = parseInt(
-    document.getElementById("position1").options[
-      document.getElementById("position1").selectedIndex
-    ].value
-  );
-  let position2 = parseInt(
-    document.getElementById("position2").options[
-      document.getElementById("position2").selectedIndex
-    ].value
-  );
-  let strategy1 = parseInt(
-    document.getElementById("strategy1").options[
-      document.getElementById("strategy1").selectedIndex
-    ].value
-  );
-  let strategy2 = parseInt(
-    document.getElementById("strategy2").options[
-      document.getElementById("strategy2").selectedIndex
-    ].value
-  );
-  let first1 = parseInt(
-    document.getElementById("first1").options[
-      document.getElementById("first1").selectedIndex
-    ].value
-  );
-  let first2 = parseInt(
-    document.getElementById("first2").options[
-      document.getElementById("first2").selectedIndex
-    ].value
-  );
-  let second1 = parseInt(
-    document.getElementById("second1").options[
-      document.getElementById("second1").selectedIndex
-    ].value
-  );
-  let second2 = parseInt(
-    document.getElementById("second2").options[
-      document.getElementById("second2").selectedIndex
-    ].value
-  );
+  let name1 = document.getElementById("jugador1").options[document.getElementById("jugador1").selectedIndex].value;
+  let name2 = document.getElementById("jugador2").options[document.getElementById("jugador2").selectedIndex].value;
+  let surf1 = document.getElementById("surface1").options[document.getElementById("surface1").selectedIndex].value;
+  let surf2 = document.getElementById("surface2").options[document.getElementById("surface2").selectedIndex].value;
+  let nomb1 = document.getElementById("jugador1").options[document.getElementById("jugador1").selectedIndex].text;
+  let nomb2 = document.getElementById("jugador2").options[document.getElementById("jugador2").selectedIndex].text;
+  let fore1 = parseInt(document.getElementById("forehand1").options[document.getElementById("forehand1").selectedIndex].value);
+  let fore2 = parseInt(document.getElementById("forehand2").options[document.getElementById("forehand2").selectedIndex].value);
+  let back1 = parseInt(document.getElementById("backhand1").options[document.getElementById("backhand1").selectedIndex].value);
+  let back2 = parseInt(document.getElementById("backhand2").options[document.getElementById("backhand2").selectedIndex].value);
+  let vol1 = parseInt(document.getElementById("volley1").options[document.getElementById("volley1").selectedIndex].value);
+  let vol2 = parseInt(document.getElementById("volley2").options[document.getElementById("volley2").selectedIndex].value);
+  let drop1 = parseInt(document.getElementById("dropshot1").options[document.getElementById("dropshot1").selectedIndex].value);
+  let drop2 = parseInt(document.getElementById("dropshot2").options[document.getElementById("dropshot2").selectedIndex].value);
+  let spe1 = parseInt(document.getElementById("speed1").options[document.getElementById("speed1").selectedIndex].value);
+  let spe2 = parseInt(document.getElementById("speed2").options[document.getElementById("speed2").selectedIndex].value);
+  let sta1 = parseInt(document.getElementById("stamina1").options[document.getElementById("stamina1").selectedIndex].value);
+  let sta2 = parseInt(document.getElementById("stamina2").options[document.getElementById("stamina2").selectedIndex].value);
+  let ser1 = parseInt(document.getElementById("service1").options[document.getElementById("service1").selectedIndex].value);
+  let ser2 = parseInt(document.getElementById("service2").options[document.getElementById("service2").selectedIndex].value);
+  let pow1 = parseInt(document.getElementById("power1").options[document.getElementById("power1").selectedIndex].value);
+  let pow2 = parseInt(document.getElementById("power2").options[document.getElementById("power2").selectedIndex].value);
+  let rest1 = parseInt(document.getElementById("resto1").options[document.getElementById("resto1").selectedIndex].value);
+  let rest2 = parseInt(document.getElementById("resto2").options[document.getElementById("resto2").selectedIndex].value);
+  let consi1 = parseInt(document.getElementById("consistencia1").options[document.getElementById("consistencia1").selectedIndex].value);
+  let consi2 = parseInt(document.getElementById("consistencia2").options[document.getElementById("consistencia2").selectedIndex].value);
+  let form1 = parseInt(document.getElementById("form1").options[document.getElementById("form1").selectedIndex].value);
+  let form2 = parseInt(document.getElementById("form2").options[document.getElementById("form2").selectedIndex].value);
+  let technique1 = parseInt(document.getElementById("technique1").options[document.getElementById("technique1").selectedIndex].value);
+  let technique2 = parseInt(document.getElementById("technique2").options[document.getElementById("technique2").selectedIndex].value);
+  let position1 = parseInt(document.getElementById("position1").options[document.getElementById("position1").selectedIndex].value);
+  let position2 = parseInt(document.getElementById("position2").options[document.getElementById("position2").selectedIndex].value);
+  let strategy1 = parseInt(document.getElementById("strategy1").options[document.getElementById("strategy1").selectedIndex].value);
+  let strategy2 = parseInt(document.getElementById("strategy2").options[document.getElementById("strategy2").selectedIndex].value);
+  let first1 = parseInt(document.getElementById("first1").options[document.getElementById("first1").selectedIndex].value);
+  let first2 = parseInt(document.getElementById("first2").options[document.getElementById("first2").selectedIndex].value);
+  let second1 = parseInt(document.getElementById("second1").options[document.getElementById("second1").selectedIndex].value);
+  let second2 = parseInt(document.getElementById("second2").options[document.getElementById("second2").selectedIndex].value);
 
   let player1 = {
     name1,
@@ -230,6 +83,7 @@ export function getPlayers() {
     second2,
   };
 
+  //console.log(player1, player2, tactic1, tactic2);
   return { player1, player2, tactic1, tactic2 };
 }
 
